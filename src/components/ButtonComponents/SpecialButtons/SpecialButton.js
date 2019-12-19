ceab27369377bb6b59968ad0291c78f130d904c4
Daniel Kanangila <daniel.kanangila@gmail.com>
2019-12-19 14:37:39 -0500

    Updated btns style

--- src/components/ButtonComponents/SpecialButtons/SpecialButton.js
+++ src/components/ButtonComponents/SpecialButtons/SpecialButton.js
@@ -3,7 +3,7 @@ import React from "react";
 const SpecialButton = (props) => {
   const { btnContent } = props;
   return (
-    <button className="btn special-btn">
+    <button className="btn btn-special">
       { btnContent }
     </button>
   );
