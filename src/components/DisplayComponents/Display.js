c35c6766167bd985e38b11b932052258cb3ebf9d
Daniel Kanangila <daniel.kanangila@gmail.com>
2019-12-22 23:34:41 -0500

    Updated display style

--- src/components/DisplayComponents/Display.js
+++ src/components/DisplayComponents/Display.js
@@ -2,10 +2,14 @@ import React from "react";
 
 const Display = (props) => {
   const { equation, result } =  props;
+  const hasError = () => {
+    return (equation.indexOf('E') !== -1 || result === 'ERROR') ?
+     ' error' : ''
+  }
   return (
-    <div className="display">
+    <div className={ `display${hasError()}` }>
       { result }
-      <span className="operation">{ equation }</span>
+      <span className={ `operation${hasError()}` }>{ equation }</span>
     </div>
   )
 };
